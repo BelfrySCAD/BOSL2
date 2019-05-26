2b2aa1496afeb98221c7b169c0083380cc94e911
Revar Desmera <revarbat@gmail.com>
2019-05-26 12:17:04 -0700

    Test script fixes.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -3,6 +3,8 @@
 OPENSCAD=/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD
 
 for testscript in tests/test_*.scad ; do
-    ${OPENSCAD} -o .off --hardwarnings --check-parameters true --check-parameter-ranges true $testscript
+    echo -n "$testscript: "
+    ${OPENSCAD} -o .off --hardwarnings --check-parameters true --check-parameter-ranges true $testscript >>/dev/null 2>&1
+    [ $? != 0 ] && echo "PASS" || echo "FAIL!"
 done
 
