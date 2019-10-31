6c282b07a9e690013cd12a88d55749677ccf3c2c
Revar Desmera <revarbat@gmail.com>
2019-10-30 18:54:03 -0700

    Fix run_tests to not leave behind .off file.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,6 +4,7 @@ OPENSCAD=/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD
 
 for testscript in tests/test_*.scad ; do
     repname="$(basename $testscript|sed 's/^test_//')"
-    ${OPENSCAD} -o .off --hardwarnings --check-parameters true --check-parameter-ranges true $testscript 2>&1 && echo "$repname: PASS" || echo -e "$repname: FAIL!\n"
+    ${OPENSCAD} -o out.echo --hardwarnings --check-parameters true --check-parameter-ranges true $testscript 2>&1 && echo "$repname: PASS" || echo -e "$repname: FAIL!\n"
+    rm -f out.echo
 done
 
