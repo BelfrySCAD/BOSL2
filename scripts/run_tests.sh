6100cae586b610b0c4543f3c65d329f20a0c050e
Revar Desmera <revarbat@gmail.com>
2019-11-05 17:31:58 -0800

    OpenSCAD cannot be relied on to return an error code on assert fail.

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -4,7 +4,15 @@ OPENSCAD=/Applications/OpenSCAD.app/Contents/MacOS/OpenSCAD
 
 for testscript in tests/test_*.scad ; do
     repname="$(basename $testscript|sed 's/^test_//')"
-    ${OPENSCAD} -o out.echo --hardwarnings --check-parameters true --check-parameter-ranges true $testscript 2>&1 && echo "$repname: PASS" || echo -e "$repname: FAIL!\n"
+    ${OPENSCAD} -o out.echo --hardwarnings --check-parameters true --check-parameter-ranges true $testscript 2>&1
+    res=$(cat out.echo)
+    if [ "$res" = "" ] ; then
+	echo "$repname: PASS"
+    else
+	echo "$repname: FAIL!"
+	cat out.echo
+	echo
+    fi
     rm -f out.echo
 done
 
